da8aa6b265916793d471cb3a65151097a6255e18
FROST <62321874+FrostCH1@users.noreply.github.com>
2020-04-07 13:26:17 +0700

    Add files via upload

---
+++ AutoJoinUI/src/AutoJoin/Main.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace AutoJoin;
+
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\utils\TextFormat as C;
+use pocketmine\utils\Config;
+
+class Main extends PluginBase implements Listener{
+	
+ public function onEnable() : void{
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);   
+		@mkdir($this->getDataFolder());
+    $this->saveDefaultConfig();
+    $this->getResource("config.yml");
+ }
+ public function onJoin(PlayerJoinEvent $event) {
+    $player = $event->getPlayer();
+$this->AutoJoin($player);
+    }
+public function AutoJoinm($player){ 
+    $api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
+    $form = $api->createSimpleForm(function (Player $player, int $data = null){
+        $result = $data;
+        if($result === null){
+            return true;
+        }             
+        switch($result){
+            case 0:                    
+            break;                     
+        }
+        });
+        $title = $this->getConfig()->get("title");
+        $content = $this->getConfig()->get("content");
+        $button = $this->getConfig()->get("button");
+        $form->setTitle($title);
+        $form->setContent($content);
+        $form->addButton($button);
+        $form->sendToPlayer($player);                  
+        return $form;                                            
+            }
+}
